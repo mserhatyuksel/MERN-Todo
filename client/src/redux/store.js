9bbf40f5450f3281d892262bc2db2879ffd65fd2
mserhatyuksel <mserhat.yksl@gmail.com>
2022-10-03 20:54:16 +0300

    delete client folder

--- client/src/redux/store.js
+++
@@ -1,9 +0,0 @@
-import { configureStore } from "@reduxjs/toolkit";
-import userSlice from "./user/userSlice";
-import todosSlice from "./todos/todosSlice";
-export const store = configureStore({
-  reducer: {
-    user: userSlice,
-    todos: todosSlice,
-  },
-});
